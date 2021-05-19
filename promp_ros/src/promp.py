3dae1d8e10eb4cf689bedcd4508bd515a30fe7c4
Rui Luo <luo.rui@husky.neu.edu>
2021-05-19 12:16:44 -0400

    added prediction

--- promp_ros/src/promp.py
+++ promp_ros/src/promp.py
@@ -2,40 +2,59 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 from scipy.stats import multivariate_normal
+from scipy.linalg import block_diag
 
 class ProMP(object):
-    def __init__(self, dt, n_samples, n_basis):
+    def __init__(self, dt, n_samples, n_basis, dof):
         self.dt = dt
         self.n_samples = n_samples
+        self.dof = dof
         self.n_basis = n_basis
         self.n_demos = 0
-        self.via_points = []
+        self.viapoints = []
 
+        # Generate Basis
         # Phi_norm: n_samples x n_basis matrix
-        self.Phi_norm = self.generate_basis(self.dt, self.n_samples, self.n_basis)
+        self.Phi_norm = self.generate_basis()
+        # diagonally concatenate the big PHI with small basis matrices
+        self.PHI = np.zeros((dof*n_samples, dof*n_basis))
+        for i in range(dof):
+            self.PHI[i*n_samples:(i+1)*n_samples, i*n_basis:(i+1)*n_basis] = self.Phi_norm
 
-        # self.import_demonstraions()
+    def main(self):
+        """
+        Main function for ProMP
+        """
         # demos: n_samples x n_demos matrix
-        self.demos = self.add_demonstration()
+        # self.demos = self.import_demonstraions()
+        # self.demos is a list containing all training trajectories
+        # each trajectory is in the format of n_samples x n_dof
+        self.demos = self.add_demonstration("training/something")
 
-        self.time_scale_trajectory()
+        # synchronize phase of demonstration trajectories
+        self.sync_demos = self.time_scale_trajectory()
+
+        # learn weight using least square regression
         # weights: a dictionary storing information for weight matrix
         self.weights = self.learn_weight()
 
-    def generate_basis(self, dt, n_samples, n_basis):
+        # update trajectory based on viapoints
+        self.traj = self.predict()
+
+    def generate_basis(self):
         """
         Generate Gaussian basis 
         """
         # set initial parameters for Gaussian basis
-        basis_center = np.linspace(0, dt*n_samples, n_basis)
+        basis_center = np.linspace(0, self.dt*self.n_samples, self.n_basis)
         sigma = 0.05
 
         # n_basis x n_samples matrix
-        basis_pdf = np.array(list(
-            map(lambda x: multivariate_normal.pdf(x=np.linspace(0,1,n_samples), mean=x, cov=sigma**2), basis_center)))
+        basis_unit = np.array(list(
+            map(lambda c: multivariate_normal.pdf(x=np.linspace(0,self.dt*(self.n_samples-1),self.n_samples), mean=c, cov=sigma**2), basis_center)))
 
         # n_samples x n_basis matrix
-        Phi = basis_pdf.T
+        Phi = basis_unit.T
 
         # normalize basis weights
         Phi_norm = Phi / np.sum(Phi, axis=1)[:, np.newaxis]
@@ -51,10 +70,11 @@ class ProMP(object):
         weights = {}
 
         # calculate weights using Ordinary Least Square estimator
-        w = np.linalg.solve(self.Phi_norm.T@self.Phi_norm, np.dot(self.Phi_norm.T, self.demos))
-        
+        w = np.linalg.solve(self.PHI.T@self.PHI, np.dot(self.PHI.T, self.sync_demos))
+
         # The weight value for each dimension is treated as a Gaussian random variable
         # The mean value is calculated across multiple demos instead of different dimensions.
+        # mean_w vector is a n_dof*n_basis x 1 vector: [w1.T, w2.T, ... w_ndof.T].T
         mean_w = np.mean(w, axis=1)
 
         # the input matrix to np.cov should follow the rule: each row contains the measurements of one variable
@@ -89,18 +109,23 @@ class ProMP(object):
         
         return Y
 
-    def import_demonstration(self):
+    def import_demonstration(self, addr):
         """
         Import demonstrations from files
         """
-        pass
+        data = []
+        for i in range(self.n_demos):
+            traj = np.loadtxt(open(addr + str(i+1) + ".csv"), delimiter=",")
+            data.append(traj)
+
+        return data
 
 
-    def time_scale_trajectory(self):
+    def time_alignment_trajectory(self):
         """
         Synchornize trajctories with same time scale
         """
-        pass
+        map(lambd)
 
     def add_viapoints(self, viapoint):
         """
@@ -126,19 +151,33 @@ class ProMP(object):
             v_mean = viapoint['mean']
             v_cov = viapoint['cov']
 
-            # retrieve the weight vector at the same phase
-            basis = self.Phi_norm[phase, :][:, np.newaxis]
+            # retrieve the weight vector at the same phase in multiple dof we are interested
+            # PHI: dof*n_sample x dof*n_basis
+            basis = self.PHI[[phase*+i*self.n_samples for i in range(self.dof)], :]
+
+            if (basis.ndim == 1):
+                basis = basis[:, np.newaxis]
+            else:
+                basis = basis.T
 
             # conditioning
             inv = np.linalg.inv(v_cov + basis.T @ cov @ basis)
+            # K: dof*n_basis x dof
             K = cov @ basis @ inv
             mean = mean + K @ (v_mean - np.dot(basis.T, mean))
             cov = cov - K @ np.dot(basis.T, cov)
 
             # we could output new mean and cov every iteration for online planning
 
-        #compute new trajectory
-        new_traj = np.dot(self.Phi_norm, mean)
+        # delete viapoints that are already conditioned
+        self.viapoints.clear()
+
+        # save new mean and cov, compute new trajectory
+        self.weights['mean_w'] = mean
+        self.weights['cov_w']  = cov
+        new_traj = np.dot(self.PHI, mean)
+        # reshape new_traj following the defined trajectory format: n_samples x n_dof
+        new_traj = new_traj.reshape((self.dof, self.n_samples)).T
         return new_traj
 
     ### define properties
