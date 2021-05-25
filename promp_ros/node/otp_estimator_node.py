7b347ce0c2c5b8b893230f1c80c122ccea5c7fb0
Rui Luo <luo.rui@husky.neu.edu>
2021-05-25 13:00:11 -0400

    added trajectory resampler

--- promp_ros/node/otp_estimator_node.py
+++ promp_ros/node/otp_estimator_node.py
@@ -6,13 +6,16 @@ from geometry_msgs.msg import Pose
 from sensor_msgs.msg import JointState
 from multiprocessing import Lock
 from apriltag_ros.msg import AprilTagDetectionArray, AprilTagDetection
-import trajectory_msgs
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 import message_filters
 import matplotlib.pyplot as plt
+import copy
 
 class OTPEstimator(object):
     def __init__(self):
+        ## Pro-MP
+        self.promp = ProMP(0.1, 20, "", 40)
+
         ## OTP calculation
         self.otp_d = Pose()
         self.otp_s = Pose()
@@ -25,7 +28,7 @@ class OTPEstimator(object):
         tag_sub = message_filters.Subscriber("/tag_detections", AprilTagDetectionArray)
         time_sync = message_filters.ApproximateTimeSynchronizer([kinova_sub, tag_sub], 10, slop=0.2)
         #debug subscriber
-        time_sync.registerCallback(self.data_filter_cb)
+        time_sync.registerCallback(self.message_sync_cb)
 
         ## ROS publishers
         self.planned_traj_pub = rospy.Publisher('/planned_trajectory', JointTrajectory, queue_size=1, latch=True)
@@ -35,8 +38,12 @@ class OTPEstimator(object):
         self.start_record = False
         self.t = 0.0
 
-        self.robot_joint_trajectory = JointTrajectory()
+        # each element of robot_joint_trajectory is a trajectory_msgs/JointTrajectoryPoint
+        self.robot_joint_trajectory = []
+        self.res_robot_joint_traj = []
+        # each element of socket trajectory is a geometry_msgs/Pose 
         self.socket_trajectory = []
+        self.res_socket_traj = []
         self.sample_time = []
 
     def compute_static_otp(self):
@@ -60,18 +67,6 @@ class OTPEstimator(object):
     def query_dynamic_otp(self):
         """
         Query the current dynamic otp from ProMP
-        """
-        pass
-
-    def human_traj_callback(self):
-        """
-        Callback function to subscribe human trajectory
-        """
-        pass
-
-    def publish_robot_traj(self):
-        """
-        Publish the robot trajectory for the next period
         """
         pass
 
@@ -100,14 +95,13 @@ class OTPEstimator(object):
         t = diff.secs * 1e3 + diff.nsecs * 1e-6
         return t
     
-    def data_filter_cb(self, robot_msg, socket_msg):
+    def message_sync_cb(self, robot_msg, socket_msg):
         """
         Synchronize robot joint states trajectory and socket pose trajectory
         """
         if (self.start_record):
             if (len(self.sample_time) == 0):
                 self.init_time = robot_msg.header.stamp
-                self.robot_joint_trajectory.joint_names = robot_msg.name[0:7]
             robot_time = robot_msg.header.stamp
             socket_time = socket_msg.header.stamp
             # add a small displacement to time
@@ -115,13 +109,17 @@ class OTPEstimator(object):
             self.sample_time.append(t)
             # self.sample_time.append(self.diff_time(socket_time, self.init_time))
 
-            # add sampled joint to trajectory
+            # add sampled robot joint to trajectory
             sample_point = JointTrajectoryPoint()
             sample_point.positions = robot_msg.position[0:7]
             sec = t / 1000
             sample_point.time_from_start = rospy.Duration(secs=sec)
 
-            self.robot_joint_trajectory.points.append(sample_point)
+            # add socket pose in kinect frame to trajectory
+            # geometry_msgs/Pose
+            self.socket_trajectory.append(socket_msg.detections[0].pose.pose.pose)
+
+            self.robot_joint_trajectory.append(sample_point)
     
     def save_training_data(self, index):
         """
@@ -130,17 +128,40 @@ class OTPEstimator(object):
         # convert robot joint trajectory into numpy array
         joint_array = np.array()
         np.savetxt('../training/plug/hrc_traj_'+index+'.csv', joint_array, delimiter=",")
-    
-    def publish_planned_trajectory(self):
-        self.robot_joint_trajectory.header.stamp = rospy.Time.now()
-        self.planned_traj_pub.publish(self.robot_joint_trajectory)
+
+    def publish_trajectory(self, traj_list):
+        traj_msg = JointTrajectory()
+        traj_msg.header.stamp = rospy.Time.now()
+        for p in traj_list:
+            traj_msg.points.append(p)
+        self.planned_traj_pub.publish(traj_msg)
 
     def record_data(self, time):
         input("Enter to start recording")
-        opte.start_record = True
+        self.start_record = True
         rospy.sleep(time)
-        opte.start_record = False
+        self.start_record = False
         rospy.loginfo("Finished recording")
+    
+    def trajectory_resampler(self, traj, f):
+        """
+        Sample trajectory at a given frequency
+        """
+        ## the default frequency of sampling data is about 30hz
+        assert(f <= 30), "the resampled frequency should be less than 30hz"
+        l = len(traj)
+        step = 30 // f
+        res_robot_joint_traj = copy.deepcopy(traj[0:l:step])
+        return res_robot_joint_traj
+    
+    def list_to_array(self):
+        # socket_pose = np.zeros((1,3))
+        # socket_pose[0] = socket_msg.detections.pose.pose.pose.position.x
+        # socket_pose[1] = socket_msg.detections.pose.pose.pose.position.y
+        # socket_pose[2] = socket_msg.detections.pose.pose.pose.position.z
+        pass
+
+    ############################DEBUG FUNCTIONS############################3
     
     def plot_traj(self, joint_trajectory, index=1):
         fig = plt.figure(index, figsize=(10,20))
@@ -186,11 +207,12 @@ if __name__ == "__main__":
     opte.record_data(2)
     # opte.plot_traj(opte.robot_joint_trajectory)
 
-    
-    opte.publish_planned_trajectory()
-    msg = rospy.wait_for_message("/smoothed_trajectory", JointTrajectory)
-    opte.plot_traj(msg, 1)
-    plt.show()
+    resampled_traj = opte.trajectory_resampler(opte.robot_joint_trajectory, 10)
+    opte.publish_trajectory(resampled_traj)
+
+    # msg = rospy.wait_for_message("/smoothed_trajectory", JointTrajectory)
+    # opte.plot_traj(msg, 1)
+    # plt.show()
 
     rospy.spin()
 
